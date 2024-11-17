c96637bce0194d68daef399f4857583d34a0925e
Yevhenii Smyrnov <kalledat57@gmail.com>
2024-11-17 23:33:46 +0200

    Implement Lab 4 (yaaaaaaay)

--- src/main/java/com/example/kpi/DbInitializer.java
+++ src/main/java/com/example/kpi/DbInitializer.java
@@ -1,13 +1,15 @@
 package com.example.kpi;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.ReadConcern;
-import com.mongodb.ReadPreference;
+import com.mongodb.*;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.FindOneAndUpdateOptions;
+import com.mongodb.connection.ClusterConnectionMode;
+import com.mongodb.connection.ClusterType;
 import com.mongodb.internal.selector.PrimaryServerSelector;
 import org.bson.BsonDocumentWrapper;
+import org.bson.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,26 +21,36 @@ public class DbInitializer implements AutoCloseable {
     private static final String DATABASE = "lab4";
     private static final String URL = "mongodb://mongo-1:27017,mongo-2:27017,mongo-3:27017";
     private final MongoClient client;
+    private MongoCollection<Document> db;
 
     public DbInitializer(TaskType taskType) {
         var settings = MongoClientSettings.builder()
-                .applyToClusterSettings(builder -> builder.serverSelector(new PrimaryServerSelector()))
-                .applyToClusterSettings(builder -> builder.serverSelectionTimeout(60, TimeUnit.SECONDS))
-                .applyToClusterSettings(builder -> builder.requiredReplicaSetName("rs0"))
-                .readPreference(ReadPreference.primary())
+                .applyToClusterSettings(builder -> builder
+                        .mode(ClusterConnectionMode.MULTIPLE)
+                        .requiredClusterType(ClusterType.REPLICA_SET)
+                        .serverSelector(new PrimaryServerSelector())
+                        .requiredReplicaSetName("rs0"))
+                .applyToSocketSettings(builder -> builder
+                        .readTimeout(1000, TimeUnit.MILLISECONDS))
+                .readPreference(ReadPreference.secondary())
                 .writeConcern(taskType.getWriteConcern())
-                .readConcern(ReadConcern.AVAILABLE)
+                .readConcern(ReadConcern.DEFAULT)
                 .applyConnectionString(new ConnectionString(URL))
                 .build();
         log.info(settings.toString());
         this.client = MongoClients.create(settings);
+        db = client.getDatabase(DATABASE).getCollection(DATABASE);
     }
 
     public void increment() {
-        client.getDatabase(DATABASE).getCollection(DATABASE)
-                .findOneAndUpdate(
-                        BsonDocumentWrapper.parse("{name:'Hello'}"),
-                        BsonDocumentWrapper.parse("{$inc:{'likes':1}}"));
+        try {
+            db.findOneAndUpdate(
+                    BsonDocumentWrapper.parse("{name:'Hello'}"),
+                    BsonDocumentWrapper.parse("{$inc:{'likes':1}}"),
+                    new FindOneAndUpdateOptions().maxTime(1, TimeUnit.SECONDS));
+        } catch (MongoSocketReadTimeoutException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 
     @Override
